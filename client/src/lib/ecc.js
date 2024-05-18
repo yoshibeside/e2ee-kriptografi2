a4e1ee057076aa8dcef71a64959b7606bb2c2a54
Yakobus Iryanto Prasethio <yakobusiryantoprasethio@gmail.com>
2024-05-18 12:40:26 +0700

    refactor: move ecc param inside class

--- client/src/lib/ecc.js
+++ client/src/lib/ecc.js
@@ -1,10 +1,25 @@
-class ECC {
-  constructor(p, a, b, G, n) {
-    this.p = p;
-    this.a = a;
-    this.b = b;
-    this.G = G;
-    this.n = n;
+export class ECC {
+  constructor() {
+    this.p = BigInt(
+      "0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff"
+    );
+    this.a = BigInt(
+      "0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc"
+    );
+    this.b = BigInt(
+      "0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b"
+    );
+    this.G = [
+      BigInt(
+        "0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296"
+      ),
+      BigInt(
+        "0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5"
+      ),
+    ];
+    this.n = BigInt(
+      "0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551"
+    );
   }
 
   mod(n, m) {
@@ -82,20 +97,3 @@ class ECC {
     return { privateKey, publicKey };
   }
 }
-
-const p = BigInt(
-  "0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff"
-);
-const a = BigInt(
-  "0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc"
-);
-const b = BigInt(
-  "0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b"
-);
-const G = [
-  BigInt("0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296"),
-  BigInt("0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5"),
-];
-const n = BigInt(
-  "0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551"
-);
